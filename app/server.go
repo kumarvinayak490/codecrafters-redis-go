7a9beae6c88c212516694cbe72ca87e318c1e293
kumarvinayak490 <vinayak@ineuron.ai>
2024-02-08 12:07:11 +0530

    pass 2nd stage deferred connection close

--- app/server.go
+++ app/server.go
@@ -12,11 +12,13 @@ func main() {
 	fmt.Println("Logs from your program will appear here!")
 
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	defer l.Close()
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
 	conn, err := l.Accept()
+	defer conn.Close()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
